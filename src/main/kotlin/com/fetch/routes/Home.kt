062552ca28c4ce922a03051134d7a030aaeee42f
David Thatcher <dtthatcher@gmail.com>
2021-12-07 13:23:17 -0500

    updated homepage

--- src/main/kotlin/com/fetch/routes/Home.kt
+++ src/main/kotlin/com/fetch/routes/Home.kt
@@ -10,10 +10,31 @@ import org.kodein.di.ktor.closestDI
 
 fun Route.Home(){
     val transactionService by closestDI().instance<TransactionService>()
-
+    val intro = """
+        Welcome to the homepage!
+        
+        The routes are as follows:
+        / = Home
+        /all = Get call to all transactions in DB ordered by timestamp
+        /add = Post call manually add new transaction
+        /spend = Put call to spend the points!!
+        
+        I recommend PostMan (or some other http handler) for accessing your post and put request.
+        Post requests respond with updated payer and points as does the Put calls.
+        
+        Enjoy!
+        
+        Here's a few preloaded payer and points totals
+        
+        ${transactionService.getCurrentPointTotal().toString()}
+        
+        and here's their transactions
+        
+        ${transactionService.getAllTransactionsByTimestamp().toString()}
+    """.trimIndent()
     route("/") {
         get {
-            call.respond(HttpStatusCode.OK, transactionService.getCurrentPointTotal().toString())
+            call.respond(HttpStatusCode.OK, intro)
         }
     }
 }
